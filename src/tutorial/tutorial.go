700d2cdc456af622fbd399771cd39d46d9433a3a
ShuaiZhang <zhangshuaiisme@gmail.com>
2019-03-31 19:56:56 +0800

    some mod

--- src/tutorial/tutorial.go
+++ src/tutorial/tutorial.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-func test_var(){
-    fmt.Println("\ntest_var ===========")
-    var s string = "hello"
-    var i int = 10
-    fmt.Println(s,i)
-}
+// my package
+import (
+    "tutorial/typeZeroVal"  //1. 测试类型零值
+)
 
 func main(){ 
     // { 不能在单独的行上
     fmt.Println("Hello, Go!")
-    test_var()
+    
+    //1. 测试类型零值
+    typeZeroVal.TestTypeZeroVal()
 }
---
+++ src/tutorial/typeZeroVal/typeZeroVal.go
@@ -0,0 +1,9 @@
+package typeZeroVal
+
+import "fmt"
+
+// TestTypeZeroVal 首字母必须大写才能对外使用
+func TestTypeZeroVal(){
+    var i int
+    fmt.Println("int:",i)
+}
