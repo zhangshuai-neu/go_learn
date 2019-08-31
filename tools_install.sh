5e1ee509a989abc9a627ff673f34cb55772156c0
zhangshuai <zhangshuaiisme@gmail.com>
2019-08-31 15:33:23 +0800

    add some submodule

--- tools_install.sh
+++ tools_install.sh
@@ -1,3 +1,5 @@
+# vs code 需要的工具链不全, 所以经将所有需要的代码手动clone了下来
+
 #! /bin/bash
 
 if ! [ -e ./src/github.com ];then
@@ -5,4 +7,13 @@ if ! [ -e ./src/github.com ];then
 fi
 cd ./src/github.com;
 
-git submodule add https://github.com/rogpeppe/godef.git src/github.com/godef
+# git submodule add https://github.com/rogpeppe/godef.git src/github.com/rogpeppe/godef
+# git submodule add https://github.com/ramya-rao-a/go-outline.git src/github.com/ramya-rao-a/go-outline
+# git submodule add https://github.com/golang/tools.git src/golang/x/tools
+# git submodule add https://github.com/sqs/goreturns src/github.com/sqs/goreturns
+git submodule add https://github.com/nsf/gocode src/github.com/nsf/gocode
+
+
+go install src/github.com/rogpeppe/godef/
+go install src/github.com/nsf/gocode/
+
