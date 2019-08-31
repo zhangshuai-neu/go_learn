6a863619e090c6a63cf2a10e5eb99b591d674678
zhangshuai <zhangshuaiisme@gmail.com>
2019-08-31 15:55:36 +0800

    add some submodule

--- tools_install.sh
+++ tools_install.sh
@@ -9,11 +9,13 @@ cd ./src/github.com;
 
 # git submodule add https://github.com/rogpeppe/godef.git src/github.com/rogpeppe/godef
 # git submodule add https://github.com/ramya-rao-a/go-outline.git src/github.com/ramya-rao-a/go-outline
-# git submodule add https://github.com/golang/tools.git src/golang/x/tools
 # git submodule add https://github.com/sqs/goreturns src/github.com/sqs/goreturns
-git submodule add https://github.com/nsf/gocode src/github.com/nsf/gocode
+# git submodule add https://github.com/nsf/gocode src/github.com/nsf/gocode
+# git submodule add https://github.com/golang/tools.git src/golang.org/x/tools
 
 
-go install src/github.com/rogpeppe/godef/
-go install src/github.com/nsf/gocode/
+go install github.com/rogpeppe/godef
+go install github.com/ramya-rao-a/go-outline
+go install github.com/nsf/gocode
+go install github.com/sqs/goreturns
 
