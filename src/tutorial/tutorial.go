fd27305dfa2ed41640506ff76c4b147734a92584
zhangshuai <zhangshuaiisme@gmail.com>
2019-09-01 15:52:51 +0800

    add some tools

--- src/tutorial/tutorial.go
+++ src/tutorial/tutorial.go
@@ -1,59 +1,60 @@
 package main
 
-import ("fmt"
+import (
+	"fmt"
 	"tutorial/testJson"
 )
 
 func main() { // { 不能在单独的行上
 	fmt.Println("Hello, Go!")
-/*
-	fmt.Println("===== 1. 测试类型零值 =====")
-	typeZeroVal.TestTypeZeroVal()
+	/*
+		fmt.Println("===== 1. 测试类型零值 =====")
+		typeZeroVal.TestTypeZeroVal()
 
-	fmt.Println("===== 2. 测试变量定义 =====")
-	varUse.TestVarUse()
+		fmt.Println("===== 2. 测试变量定义 =====")
+		varUse.TestVarUse()
 
-	fmt.Println("===== 3. 测试常量使用 =====")
-	constUse.TestConstUse()
+		fmt.Println("===== 3. 测试常量使用 =====")
+		constUse.TestConstUse()
 
-	fmt.Println("===== 4. 测试++,--和指针 =====")
-	ptr.TestPtr()
+		fmt.Println("===== 4. 测试++,--和指针 =====")
+		ptr.TestPtr()
 
-	fmt.Println("===== 5. 测试条件表达式 =====")
-	ifExpression.TestIfExpression()
+		fmt.Println("===== 5. 测试条件表达式 =====")
+		ifExpression.TestIfExpression()
 
-	fmt.Println("===== 6. 测试循环表达式 =====")
-	forExpression.TestForExpression()
+		fmt.Println("===== 6. 测试循环表达式 =====")
+		forExpression.TestForExpression()
 
-	fmt.Println("===== 7. 测试switch表达式 =====")
-	switchExpression.TestSwitchExpression()
+		fmt.Println("===== 7. 测试switch表达式 =====")
+		switchExpression.TestSwitchExpression()
 
-	fmt.Println("===== 8. 跳转测试 =====")
-	jumpLabel.TestJumpLabel()
+		fmt.Println("===== 8. 跳转测试 =====")
+		jumpLabel.TestJumpLabel()
 
-	fmt.Println("===== 9. 数组测试 =====")
-	array.TestArray()
+		fmt.Println("===== 9. 数组测试 =====")
+		array.TestArray()
 
-	fmt.Println("===== 10. slice测试 =====")
-	testSlice.TestSlice()
+		fmt.Println("===== 10. slice测试 =====")
+		testSlice.TestSlice()
 
-	fmt.Println("===== 11. channel测试 =====")
-	testChannel.TestChannel()
+		fmt.Println("===== 11. channel测试 =====")
+		testChannel.TestChannel()
 
-	fmt.Println("===== 12. goroutine测试 =====")
-	testGoroutine.TestGorourine()
+		fmt.Println("===== 12. goroutine测试 =====")
+		testGoroutine.TestGorourine()
 
-	fmt.Println("===== 13. struct测试 =====")
-	testStruct.TestStruct()
+		fmt.Println("===== 13. struct测试 =====")
+		testStruct.TestStruct()
 
-	fmt.Println("===== 14. 左值函数测试 =====")
-	testFunc.TestFunc()
+		fmt.Println("===== 14. 左值函数测试 =====")
+		testFunc.TestFunc()
 
-	fmt.Println("====== 15. 测试string =====")
-	testString.TestString()
+		fmt.Println("====== 15. 测试string =====")
+		testString.TestString()
 
-	fmt.Println("====== 16. 测试time =====")
-	testTime.TestTime()
+		fmt.Println("====== 16. 测试time =====")
+		testTime.TestTime()
 	*/
 
 	fmt.Println("====== 17. 测试json=====")
--- tools_install.sh
+++ tools_install.sh
@@ -13,10 +13,21 @@
 # git submodule add https://github.com/sqs/goreturns src/github.com/sqs/goreturns
 # git submodule add https://github.com/nsf/gocode src/github.com/nsf/gocode
 # git submodule add https://github.com/golang/tools.git src/golang.org/x/tools
+# git submodule add https://github.com/kisielk/errcheck.git src/github.com/kisielk/errcheck
+# git submodule add https://github.com/golang/net.git src/golang.org/x/net
+# git submodule add https://github.com/golang/xerrors.git src/golang.org/x/xerrors
+# git submodule add https://github.com/golang/sync.git src/golang.org/x/sync
+
 
 
 go install github.com/rogpeppe/godef
 go install github.com/ramya-rao-a/go-outline
 go install github.com/nsf/gocode
 go install github.com/sqs/goreturns
+go install github.com/kisielk/errcheck
 
+go install golang.org/x/tools/cmd/goimports
+go install golang.org/x/tools/cmd/godoc
+go install golang.org/x/tools/cmd/gopls
+go install golang.org/x/tools/cmd/gorename
+go install golang.org/x/tools/cmd/gotype
