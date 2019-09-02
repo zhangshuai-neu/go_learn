5dec5a3a70b2be74e641b69e5260f9e523c5afdb
zhangshuai <zhangshuaiisme@gmail.com>
2019-09-02 15:25:17 +0800

    test namespace

--- src/mydocker/test/test_namespace.go
+++ src/mydocker/test/test_namespace.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
 
+func main() {
+	cmd := exec.Command("sh")
+	// 配置namespace
+	cmd.SysProcAttr.Cloneflags |= syscall.CLONE_NEWUTS
+	cmd.SysProcAttr.Cloneflags |= syscall.CLONE_NEWIPC
+	cmd.SysProcAttr.Cloneflags |= syscall.CLONE_NEWPID
+	cmd.SysProcAttr.Cloneflags |= syscall.CLONE_NEWNS
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
